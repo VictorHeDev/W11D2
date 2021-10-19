983fb7e2c94e80d1096123da452025b8b154be18
Woodstock <victorhe97@gmail.com>
2021-10-19 12:12:22 -0400

    Initial functionality of store, reducer, and actions completed. Can output to console.

---
+++ todos/frontend/reducers/todos_reducer.js
@@ -0,0 +1,35 @@
+import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
+
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false,
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true,
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
+  // Object.freeze(state);
+  // const nextState
+
+  switch (action.type) {
+    case RECEIVE_TODOS:
+    // state.todos = acti
+
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
+
+// Return the initial state if the state argument is undefined.
+// Return the state if the reducer doesn't care about the action.
+// Return a new state object if the reducer cares about the action.
--- todos/frontend/todo_redux.jsx
+++ todos/frontend/todo_redux.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import configureStore from './store/store';
 
 render(<h1>Hello world</h1>, document.getElementById("root"));
+window.store = configureStore();
