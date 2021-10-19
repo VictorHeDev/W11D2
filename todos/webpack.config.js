2c496e414f25f30585cfdb818be48e7651a6478a
Mansoo Kim <mansookim09@gmail.com>
2021-10-19 11:42:36 -0400

    Finish basic setup of files

---
+++ todos/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./frontend/todo_redux.jsx",
+  output: {
+      path: path.resolve(__dirname),
+      filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', ['@babel/react', {
+              "runtime": "automatic"
+            }]]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  devtool: 'source-map'
+};
