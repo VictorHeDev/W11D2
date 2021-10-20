05ad23d8d910513462e6b36ec29c4f4ab0d61477
Mansoo Kim <mansookim09@gmail.com>
2021-10-20 14:09:41 -0400

    Add toggling done status

--- todos/frontend/components/todo_list/TodoListItem.jsx
+++ todos/frontend/components/todo_list/TodoListItem.jsx
@@ -1,13 +1,19 @@
 const TodoListItem = ({ todo, receiveTodo, removeTodo }) => {
-  const handleDelete = e => {
-    e.preventDefault();
+  const handleDelete = () => {
     removeTodo(todo);
+  }
+
+  const toggleTodo = () => {
+    todo.done = !todo.done;
+    receiveTodo(todo);
   }
 
   return (
     <li>
       {todo.title}
       <button onClick={handleDelete}>Delete</button>
+      <button onClick={toggleTodo}>Toggle Status</button>
+      {todo.done ? "Done" : "Not done"}
     </li>
   )
 }
