6dfe8bf6e1e24e6add0155c01521ddb5fedb07ac
Woodstock <victorhe97@gmail.com>
2021-10-21 13:57:49 -0400

    Created and set up DB. Completed Todo migration, and wrote validations.

---
+++ todos_app/db/migrate/20211021175056_create_todos.rb
@@ -0,0 +1,13 @@
+class CreateTodos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :todos do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.boolean :done, null: false, default: false
+
+      t.timestamps
+    end
+    add_index :todos, :title
+
+  end
+end
