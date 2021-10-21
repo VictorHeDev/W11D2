17884ba027662db562d5d0dafa91b51e96fcccfb
Woodstock <victorhe97@gmail.com>
2021-10-21 14:11:35 -0400

    Finished the todos controller and created the routes.

---
+++ todos_app/app/controllers/api/todos_controller.rb
@@ -0,0 +1,39 @@
+class Api::TodosController < ApplicationController
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def index
+    render json: Todo.all
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @todo = Todo.find_by(id: params[:id])
+
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @todo = Todo.find_by(id: params[:id])
+    @todo.destroy if @todo
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :body, :done)
+  end
+
+end
